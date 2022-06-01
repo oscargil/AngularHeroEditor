f5ccb0428ccb712db591faad60cb2c1e67426ec1
Oscar Gil <oscargil@MacBook-Pro-de-Admin.local>
2022-06-01 16:00:11 +0200

    Observable implementation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,14 +2,16 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 
   constructor() { }

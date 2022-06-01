7dbad79add596eaf43fde01f9c9bb6c12e695e41
Oscar Gil <oscargil@MacBook-Pro-de-Admin.local>
2022-06-01 15:52:25 +0200

    HeroService is ready

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,16 @@
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 
   constructor() { }
 }
